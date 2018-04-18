cd664398142d131df30e37c71f24829e6f9bd211
Vijay Oommen <vijayoommen@ymail.com>
2018-04-18 18:52:43 -0400

    2nd commit

---
+++ app_server/controllers/locations.js
@@ -0,0 +1,12 @@
+/* GET Locations page */
+module.exports.homelist = function(req, res){
+	res.render('index', { title: 'Home' });	
+};
+
+module.exports.locationInfo = function(req, res){
+	res.render('index', { title: 'Location Info' });	
+};
+
+module.exports.addReview = function(req, res){
+	res.render('index', { title: 'Add Review' });	
+};
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,12 +1,23 @@
 var express = require('express');
 var router = express.Router();
-var ctrlMain = require('../controllers/main.js');
+var ctrlMain = require('../controllers/main');
+var ctrlLocations = require('../controllers/locations');
+var ctrlOthers = require('../controllers/others');
+
 
 var homepageController = function(req, res){
 	ctrlMain.index(req,res);
 };
 
 /* GET home page. */
-router.get('/', homepageController);
+//router.get('/', homepageController);
+
+/* Locations pages */
+router.get('/', ctrlLocations.homelist);
+router.get('/location', ctrlLocations.locationInfo);
+router.get('/location/review/new', ctrlLocations.addReview);
+
+/* Others pages */
+router.get('/about', ctrlOthers.about);
 
 module.exports = router;
