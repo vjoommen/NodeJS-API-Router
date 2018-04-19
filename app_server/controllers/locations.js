60dd073997ed8d9e9cd134698ce94a6e12f93c05
Vijay Oommen <vijayoommen@ymail.com>
2018-04-19 08:06:48 -0400

    3rdcommit

--- app_server/controllers/locations.js
+++ app_server/controllers/locations.js
@@ -1,12 +1,34 @@
 /* GET Locations page */
 module.exports.homelist = function(req, res){
-	res.render('index', { title: 'Home' });	
+	res.render('locations-list', {
+	 title: 'Location List',
+	 pageHeader: {
+	 	title: 'Loc8r',
+	 	strapline: 'Find places to work with wifi near you !!'
+	 },
+	 locations: [
+	 {
+	 	name: 'Starcups',
+	 	address: '125 High St, Reading, US',
+	 	rating: 3,
+	 	facilities: ['Hot drinks','Food','Wifi'],
+	 	distance: '100m'
+	 },
+	 {
+	 	name: 'Coffee Cups',
+	 	address: '323 Main St, Hihligf, US',
+	 	rating: 1,
+	 	facilities: ['Hot drinks','Drinks','Wifi'],
+	 	distance: '222m'
+	 }
+	 ] 
+	});	
 };
 
 module.exports.locationInfo = function(req, res){
-	res.render('index', { title: 'Location Info' });	
+	res.render('location-info', { title: 'Location Info' });	
 };
 
 module.exports.addReview = function(req, res){
-	res.render('index', { title: 'Add Review' });	
+	res.render('location-review-form', { title: 'Add Review' });	
 };
